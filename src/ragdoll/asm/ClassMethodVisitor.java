516a869f0641dd9d2cfe768104a7ac58be07f4ec
zxqdx <zxq001zxq001@gmail.com>
2015-12-15 21:58:29 -0500

    Added inclass code

---
+++ src/ragdoll/asm/ClassMethodVisitor.java
@@ -0,0 +1,52 @@
+package ragdoll.asm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+
+public class ClassMethodVisitor extends ClassVisitor {
+
+	public ClassMethodVisitor(int arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ClassMethodVisitor(int arg0, ClassVisitor arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+		// TODO Auto-generated method stub
+		MethodVisitor toDecorate = super.visitMethod(access, name, desc, signature, exceptions);
+
+		String returnType = Type.getReturnType(desc).getClassName();
+
+		Type[] argTypes = Type.getArgumentTypes(desc);
+		List<String> sTypes = new ArrayList<>();
+		for (Type t : argTypes) {
+			sTypes.add(t.getClassName());
+		}
+
+		String level = "";
+		if ((access & Opcodes.ACC_PUBLIC) != 0) {
+			level = "public";
+		} else if ((access & Opcodes.ACC_PROTECTED) != 0) {
+			level = "protected";
+		} else if ((access & Opcodes.ACC_PRIVATE) != 0) {
+			level = "private";
+		} else {
+			level = "default";
+		}
+
+		System.out.println("Method " + level + "	" + returnType + "	" + name + "	" + sTypes.toString());
+
+		return toDecorate;
+	}
+
+}
---
+++ src/ragdoll/asm/FirstASM.java
@@ -0,0 +1,19 @@
+package ragdoll.asm;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+public class FirstASM {
+
+	public static void main(String[] args) throws IOException {
+		ClassReader reader = new ClassReader("ragdoll.asm.FirstASM");
+		ClassVisitor visitor = new TraceClassVisitor(new PrintWriter(System.out));
+		reader.accept(visitor, ClassReader.EXPAND_FRAMES);
+
+	}
+
+}
