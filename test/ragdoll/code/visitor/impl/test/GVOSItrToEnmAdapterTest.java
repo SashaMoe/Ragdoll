7df4a1bdacc31b601a4f16a5f7fb8309d97332fc
zxqdx <zxq001zxq001@gmail.com>
2016-01-29 00:47:24 -0500

    [Meow] Added Automated tests for adapter and decorator patterns.

---
+++ test/ragdoll/code/visitor/impl/test/GVOSItrToEnmAdapterTest.java
@@ -0,0 +1,151 @@
+package ragdoll.code.visitor.impl.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Opcodes;
+
+import ragdoll.app.pattern.AdapterPattern;
+import ragdoll.app.pattern.DecoratorPattern;
+import ragdoll.app.pattern.GVFormatConsumer;
+import ragdoll.app.pattern.IFormatConsumer;
+import ragdoll.app.pattern.SingletonPattern;
+import ragdoll.asm.uml.ClassDeclarationVisitor;
+import ragdoll.asm.uml.ClassFieldVisitor;
+import ragdoll.asm.uml.ClassMethodVisitor;
+import ragdoll.code.uml.api.IClass;
+import ragdoll.code.uml.api.IClassDeclaration;
+import ragdoll.code.uml.impl.Klass;
+import ragdoll.code.uml.pattern.APatternDetector;
+import ragdoll.code.uml.pattern.PatternController;
+import ragdoll.code.visitor.impl.GVOutputStream;
+import ragdoll.util.ClassFinder;
+
+public class GVOSItrToEnmAdapterTest {
+
+	private GVOutputStream gvOS;
+	private final Map<String, IClass> iClasses;
+	private StringBuffer sb;
+
+	public GVOSItrToEnmAdapterTest() throws IOException, ClassNotFoundException {
+		String packageName = "ragdoll.asm.uml.test.sample.adapter,java.util.Enumeration,java.util.Iterator";
+		List<Class<?>> classes = ClassFinder.find(packageName);
+		List<String> classNames = new ArrayList<>();
+		for (Class<?> c : classes) {
+			classNames.add(c.getName());
+		}
+
+		GVOutputStream gvOS = new GVOutputStream();
+		iClasses = new HashMap<>();
+
+		for (String className : classNames) {
+			IClass newClass = new Klass(className, iClasses);
+			ClassReader reader = new ClassReader(className);
+
+			ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, newClass);
+			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, newClass);
+			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, newClass);
+
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+			iClasses.put(className, newClass);
+		}
+
+		for (String className : iClasses.keySet()) {
+			IClass klass = iClasses.get(className);
+			String superClassName = klass.getDeclaration().getNameOfSuperClass();
+			IClass superClass = iClasses.get(superClassName);
+			if (superClass != null) {
+				superClass.addSubClasses(className);
+			}
+			List<String> interfaceNames = klass.getDeclaration().getNameOfInterfaces();
+			if (interfaceNames != null) {
+				for (String interfaceName : interfaceNames) {
+					if (iClasses.containsKey(interfaceName)) {
+						IClass itf = iClasses.get(interfaceName);
+						itf.addSubClasses(className);
+					}
+				}
+			}
+		}
+
+		// Pattern Detection
+		PatternController patternController = new PatternController();
+		patternController.setClasses(iClasses);
+
+		APatternDetector singletonPattern = new SingletonPattern(patternController);
+		patternController.registerPatternDetector("singleton", singletonPattern);
+		APatternDetector adapterPattern = new AdapterPattern(patternController);
+		patternController.registerPatternDetector("adapter", adapterPattern);
+		APatternDetector decoratorPattern = new DecoratorPattern(patternController);
+		patternController.registerPatternDetector("decorator", decoratorPattern);
+
+		IFormatConsumer gvFormatConsumer = GVFormatConsumer.getInstance();
+		patternController.registerFormatConsumer(gvFormatConsumer);
+
+		patternController.detectAllPatterns();
+
+	}
+
+	private final void appendBufferLine(String s) {
+		sb.append(s + "\n");
+	}
+
+	private final void appendBuffer(String s) {
+		sb.append(s);
+	}
+
+	@Before
+	public void setUpGvOS() {
+		gvOS = new GVOutputStream();
+		sb = new StringBuffer();
+	}
+
+	@Test
+	public void testItrToEnmAdapter() {
+		IClass klass = iClasses.get("ragdoll.asm.uml.test.sample.adapter.IteratorToEnumerationAdapter");
+		klass.accept(gvOS);
+
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.adapter.IteratorToEnumerationAdapter\" [");
+		appendBufferLine("color=black");
+		appendBufferLine("fillcolor=\"#A32B2E\"");
+		appendBufferLine("style=filled");
+		appendBufferLine("label = \"{ragdoll.asm.uml.test.sample.adapter.IteratorToEnumerationAdapter");
+		appendBufferLine(
+				"«Adapter»\\n|- itr : java.util.Iterator\\l|+ hasMoreElements(): boolean\\l+ nextElement(): Object\\l}\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"dashed\"");
+		appendBufferLine("arrowhead = \"empty\"");
+		appendBufferLine("]");
+		appendBufferLine(
+				"\"ragdoll.asm.uml.test.sample.adapter.IteratorToEnumerationAdapter\" -> \"java.util.Enumeration\"");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"empty\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"dashed\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("label = \"adapts\"");
+		appendBufferLine("]");
+		appendBufferLine(
+				"\"ragdoll.asm.uml.test.sample.adapter.IteratorToEnumerationAdapter\" -> \"java.util.Iterator\"");
+		appendBufferLine("edge [label=\" \"]");
+
+		assertEquals(sb.toString(), gvOS.toString());
+	}
+
+}
---
+++ test/ragdoll/code/visitor/impl/test/GVOSTextEditDecoratorTest.java
@@ -0,0 +1,193 @@
+package ragdoll.code.visitor.impl.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.Opcodes;
+
+import ragdoll.app.pattern.AdapterPattern;
+import ragdoll.app.pattern.DecoratorPattern;
+import ragdoll.app.pattern.GVFormatConsumer;
+import ragdoll.app.pattern.IFormatConsumer;
+import ragdoll.app.pattern.SingletonPattern;
+import ragdoll.asm.uml.ClassDeclarationVisitor;
+import ragdoll.asm.uml.ClassFieldVisitor;
+import ragdoll.asm.uml.ClassMethodVisitor;
+import ragdoll.code.uml.api.IClass;
+import ragdoll.code.uml.api.IClassDeclaration;
+import ragdoll.code.uml.impl.Klass;
+import ragdoll.code.uml.pattern.APatternDetector;
+import ragdoll.code.uml.pattern.PatternController;
+import ragdoll.code.visitor.impl.GVOutputStream;
+import ragdoll.util.ClassFinder;
+
+public class GVOSTextEditDecoratorTest {
+
+	private GVOutputStream gvOS;
+	private final Map<String, IClass> iClasses;
+	private StringBuffer sb;
+
+	public GVOSTextEditDecoratorTest() throws IOException, ClassNotFoundException {
+		String packageName = "ragdoll.asm.uml.test.sample.decorator,java.io.FilterOutputStream,java.io.OutputStream,java.io.FilterInputStream,java.io.InputStream";
+		List<Class<?>> classes = ClassFinder.find(packageName);
+		List<String> classNames = new ArrayList<>();
+		for (Class<?> c : classes) {
+			classNames.add(c.getName());
+		}
+		
+		GVOutputStream gvOS = new GVOutputStream();
+		iClasses = new HashMap<>();
+
+		for (String className : classNames) {
+			IClass newClass = new Klass(className, iClasses);
+			ClassReader reader = new ClassReader(className);
+
+			ClassVisitor declVisitor = new ClassDeclarationVisitor(Opcodes.ASM5, newClass);
+			ClassVisitor fieldVisitor = new ClassFieldVisitor(Opcodes.ASM5, declVisitor, newClass);
+			ClassVisitor methodVisitor = new ClassMethodVisitor(Opcodes.ASM5, fieldVisitor, newClass);
+
+			reader.accept(methodVisitor, ClassReader.EXPAND_FRAMES);
+			iClasses.put(className, newClass);
+		}
+
+		for (String className : iClasses.keySet()) {
+			IClass klass = iClasses.get(className);
+			String superClassName = klass.getDeclaration().getNameOfSuperClass();
+			IClass superClass = iClasses.get(superClassName);
+			if (superClass != null) {
+				superClass.addSubClasses(className);
+			}
+			List<String> interfaceNames = klass.getDeclaration().getNameOfInterfaces();
+			if (interfaceNames != null) {
+				for (String interfaceName : interfaceNames) {
+					if (iClasses.containsKey(interfaceName)) {
+						IClass itf = iClasses.get(interfaceName);
+						itf.addSubClasses(className);
+					}
+				}
+			}
+		}
+
+		// Pattern Detection
+		PatternController patternController = new PatternController();
+		patternController.setClasses(iClasses);
+
+		APatternDetector singletonPattern = new SingletonPattern(patternController);
+		patternController.registerPatternDetector("singleton", singletonPattern);
+		APatternDetector adapterPattern = new AdapterPattern(patternController);
+		patternController.registerPatternDetector("adapter", adapterPattern);
+		APatternDetector decoratorPattern = new DecoratorPattern(patternController);
+		patternController.registerPatternDetector("decorator", decoratorPattern);
+
+		IFormatConsumer gvFormatConsumer = GVFormatConsumer.getInstance();
+		patternController.registerFormatConsumer(gvFormatConsumer);
+
+		patternController.detectAllPatterns();
+
+	}
+
+	private final void appendBufferLine(String s) {
+		sb.append(s + "\n");
+	}
+
+	private final void appendBuffer(String s) {
+		sb.append(s);
+	}
+
+	@Before
+	public void setUpGvOS() {
+		gvOS = new GVOutputStream();
+		sb = new StringBuffer();
+	}
+
+	@Test
+	public void testEncryptionOSDecorator() {
+		IClass klass = iClasses.get("ragdoll.asm.uml.test.sample.decorator.EncryptionOutputStream");
+		klass.accept(gvOS);
+		
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.EncryptionOutputStream\" [");
+		appendBufferLine("color=black");
+		appendBufferLine("fillcolor=\"green\"");
+		appendBufferLine("style=filled");
+		appendBufferLine("label = \"{ragdoll.asm.uml.test.sample.decorator.EncryptionOutputStream");
+		appendBufferLine("«Decorator»\\n|- encryptor : ragdoll.asm.uml.test.sample.decorator.IEncryption\\l|+ write(i0 : int): void\\l}\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"dashed\"");
+		appendBufferLine("arrowhead = \"empty\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"empty\"");
+		appendBufferLine("]");
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.EncryptionOutputStream\" -> \"java.io.FilterOutputStream\"");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"dashed\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("label = \" \"");
+		appendBufferLine("]");
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.EncryptionOutputStream\" -> \"ragdoll.asm.uml.test.sample.decorator.IEncryption\"");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("label = \"decorate\"");
+		appendBufferLine("]");
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.EncryptionOutputStream\" -> \"java.io.OutputStream\"");
+		appendBufferLine("edge [label=\" \"]");
+		
+		assertEquals(sb.toString(), gvOS.toString());
+	}
+	
+	@Test
+	public void testDecryptionOSDecorator() {
+		IClass klass = iClasses.get("ragdoll.asm.uml.test.sample.decorator.DecryptionInputStream");
+		klass.accept(gvOS);
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.DecryptionInputStream\" [");
+		appendBufferLine("color=black");
+		appendBufferLine("fillcolor=\"green\"");
+		appendBufferLine("style=filled");
+		appendBufferLine("label = \"{ragdoll.asm.uml.test.sample.decorator.DecryptionInputStream");
+		appendBufferLine("«Decorator»\\n|- decryptor : ragdoll.asm.uml.test.sample.decorator.IDecryption\\l|+ read(): int\\l+ read(b0 : byte[], i1 : int, i2 : int): int\\l}\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"dashed\"");
+		appendBufferLine("arrowhead = \"empty\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"empty\"");
+		appendBufferLine("]");
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.DecryptionInputStream\" -> \"java.io.FilterInputStream\"");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"dashed\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("]");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("label = \" \"");
+		appendBufferLine("]");
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.DecryptionInputStream\" -> \"ragdoll.asm.uml.test.sample.decorator.IDecryption\"");
+		appendBufferLine("edge [");
+		appendBufferLine("style = \"solid\"");
+		appendBufferLine("arrowhead = \"vee\"");
+		appendBufferLine("label = \"decorate\"");
+		appendBufferLine("]");
+		appendBufferLine("\"ragdoll.asm.uml.test.sample.decorator.DecryptionInputStream\" -> \"java.io.InputStream\"");
+		appendBufferLine("edge [label=\" \"]");
+		assertEquals(sb.toString(), gvOS.toString());
+	}
+}
